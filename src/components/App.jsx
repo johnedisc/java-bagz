88ef5b8372a378a011187530d4421058d050612f
johnedisc <christopher.johnedis@gmail.com>
2023-04-21 09:56:56 -0700

    create Navbar and footer components

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import './../App.css'
+import { Navbar } from './Navbar';
+import { Footer } from './Navbar';
+
+function App() {
+
+  return (
+    <>
+      <div className='container flexed-down'>        
+        <Navbar />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function Navbar(){
+  return (
+    <>
+    <div className='flexed-row'>
+      <h1>java bagz</h1>
+    </div>
+    </>
+  );
+}
+
+export function Footer(){
+  return (
+    <>
+    <div className='flexed-row'>
+      <h3>footer</h3>
+    </div>
+    </>
+  );
+}
