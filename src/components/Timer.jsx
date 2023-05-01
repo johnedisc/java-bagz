a495a17b6bf57117fdd3e92d99be82bfcbc95a32
johnedisc <christopher.johnedis@gmail.com>
2023-04-30 22:28:44 -0700

    add useTimer.js, update Timer.jsx
    create custom hook

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,21 +1,7 @@
-import { useState, useEffect } from "react";
+import { useTimer } from "./../hooks/useTimer";
 
 export const Timer = () => {
-  const [isActive, setIsActive] = useState(false);
-  const [timer, setTimer] = useState(0);
-
-  useEffect(() => {
-    let interval;
-
-    if (isActive) {
-      interval = setInterval(() => {
-        setTimer(timerState => timerState + 1)
-      }, 1000);
-    }
-
-    return () => clearInterval(interval);
-  }, [isActive]);
-
+  const [isActive, timer, setIsActive] = useTimer();
   return (
     <>
       {isActive ? <h1>{timer}</h1> : <h1>timer stopped</h1>}
---
+++ src/hooks/useTimer.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+export const useTimer = () => {
+  const [isActive, setIsActive] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimer(timerState => timerState + 1)
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+  return [isActive, timer, setIsActive];
+
+}
